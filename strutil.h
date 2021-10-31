40d485d5470d687c996a4c604510df3565081488
christinanz0523 <21900266@handong.edu>
2021-11-01 00:07:30 +0900

    Create strutil.h

---
+++ strutil.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::string trim(const std::string &s);
+std::vector<std::string> split(const std::string& i_str, const std::string& i_delim);
