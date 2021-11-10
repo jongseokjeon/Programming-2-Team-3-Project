4c22ae38f6579716a49044116f979654390c4c61
christinanz0523 <21900266@handong.edu>
2021-11-11 00:39:53 +0900

    Delete strutil.h

--- strutil.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-std::string trim(const std::string &s);
-std::vector<std::string> split(const std::string& i_str, const std::string& i_delim);
